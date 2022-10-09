a48273d913bd1b5cd60368b943389d3683cbe098
iangolovanov <ian.golovanov@stud.fh-campuswien.ac.at>
2022-10-09 20:05:54 +0200

    every task except num.4 completed

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,17 +6,64 @@ public class App {
 
     //todo Task 1
     public void largestNumber(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 1;
+        double highest = 0;
+
+        System.out.print("Number " + count + ": ");
+        double number = scanner.nextDouble(); //decimals
+
+        if (number <= 0){ //check whether the first number is smaller/equal 0
+            System.out.print("No number entered." + System.lineSeparator());
+        } else {
+            do {
+                if (number > highest) {
+                    highest = 0 + number;
+                }
+                {
+                    count++;
+                    System.out.print("Number " + count + ": ");
+                    number = scanner.nextDouble();
+                }
+            } while (number > 0);
+            System.out.printf("The largest number is %.2f", highest);
+            System.out.println();
+        }
     }
 
     //todo Task 2
-    public void stairs(){
-        // input your solution here
+    public void stairs() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a = 1;
+        System.out.print("n: ");
+
+        if (n <= 0) {
+            System.out.println("Invalid number!");
+        } else {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <= i; j++) {
+                    int num = a++;
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+            }
+        }
     }
 
     //todo Task 3
     public void printPyramid(){
-        // input your solution here
+        int a = 6;
+        for (int i = 1; i <= a; i++) {
+            for (int j = 0; j < a - i; j++) {
+                System.out.print(" ");
+            }
+            for (int s = 0; s < i * 2 - 1; s++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     //todo Task 4
@@ -26,12 +73,62 @@ public class App {
 
     //todo Task 5
     public void marks(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int negative = 0;
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 1; i > 0; i++) {
+            System.out.print("Mark " + i + ": ");
+            int mark = scanner.nextInt();
+
+            if (mark == 5) {
+                sum = sum + mark;
+                negative = negative + 1;
+                count = count + 1;
+            } else if (mark < 0 || mark > 5) { // <0 weil wir schon die Bedingung == 0 haben
+                System.out.println("Invalid mark!");
+                i = i - 1;
+            } else {
+                sum = sum + mark;
+                count = count + 1;
+            }
+            if (mark == 0 && i == 1) {
+                System.out.println("Average: 0.00");
+                System.out.println("Negative marks: 0");
+                break;
+            } else if (mark == 0) {
+                count = count - 1;
+                double average = (double) sum / count;
+                System.out.printf("Average: %.2f", average);
+                System.out.println();
+                System.out.print("Negative marks: " + negative);
+                System.out.println();
+                break;
+            }
+        }
     }
 
     //todo Task 6
     public void happyNumbers(){
-        // input your solution here
+        System.out.print("n: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int result = 0;
+        while (result != 1 && result != 4){
+            result = 0;
+            while (num != 0) {
+                int digit = num % 10;
+                result = result + digit * digit;
+                num = num / 10;
+            }
+            num = result;
+        }
+        if (result == 1) {
+            System.out.print("Happy number!" + System.lineSeparator());
+        } else {
+            System.out.print("Sad number!" + System.lineSeparator());
+        }
     }
 
     public static void main(String[] args){
